da347057cca3618bf1770909f148da767353a93e
Mohammad Zaid <64780229+them7d@users.noreply.github.com>
2025-05-21 01:18:47 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <>
       <Page />
-    <!-- Cloudflare Web Analytics -->
+    { /* <!-- Cloudflare Web Analytics --> */}
     <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "c40acaefcaf74b54a1785e9222c08ae2"}'></script>
-    <!-- End Cloudflare Web Analytics -->
+  { /* <!-- End Cloudflare Web Analytics --> */}
     </>
   )
 }
